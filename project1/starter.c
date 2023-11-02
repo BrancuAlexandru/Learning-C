4eef95bc2aa93006b0e5fd5c071d812063356a12
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-11-02 17:06:57 +0200

    Added asking for input

--- project1/starter.c
+++ project1/starter.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main () {
-  char character = '!';
-  int asciiCode = (char)character;
-  printf("Hello World%c\nchar code = %d", character, asciiCode);
+  int numberOfLoops;
+  printf("How many times do you want this to loop? ");
+  scanf("%d", &numberOfLoops);
+  
+  int inputNumber;
+  printf("What number do you want to repeat? ");
+  scanf("%d", &inputNumber);
+
+  while (numberOfLoops > 0) {
+    printf("%d\n", inputNumber);
+    numberOfLoops--;
+  }
+
+  while(1) {}
 }
