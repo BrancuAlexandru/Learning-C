a9707127b1c44938958ffaf825def6d5f3e123fc
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-11-05 22:50:01 +0200

    Added program exit command, also repeating loop to re-use the binary.

--- project1/starter.c
+++ project1/starter.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
+#include <conio.h>
 
-int main () {
-  int numberOfLoops;
-  printf("How many times do you want this to loop? ");
-  scanf("%d", &numberOfLoops);
-  
-  int inputNumber;
-  printf("What number do you want to repeat? ");
-  scanf("%d", &inputNumber);
+void main () {
 
-  while (numberOfLoops > 0) {
-    printf("%d\n", inputNumber);
-    numberOfLoops--;
+  while (1) {
+    int numberOfLoops;
+    printf("How many times do you want this to loop? ");
+    scanf("%d", &numberOfLoops);
+    
+    int inputNumber;
+    printf("What number do you want to repeat? ");
+    scanf("%d", &inputNumber);
+
+    while (numberOfLoops > 0) {
+      printf("%d\n", inputNumber);
+      numberOfLoops--;
+    }
+
+    printf("Press any key to retry or X to exit the program.\n");
+
+    char userInput = getch();
+
+    if (userInput == 'x' || userInput == 'X') {
+      break;
+    }
   }
-
-  while(1) {}
 }
