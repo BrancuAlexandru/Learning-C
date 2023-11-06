c3e3e357b4aa3e89fa0dc0bb064ff6a70041633a
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-11-06 16:38:55 +0200

    Added error handling + max input values.

--- project1/starter.c
+++ project1/starter.c
@@ -1,28 +1,62 @@
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
+
+char loop (int numberOfLoops) {
+  char inputStr[20];
+
+  while (1) {
+    printf("\nWhat word do you want to repeat? (Max 20 characters) ");
+    scanf("%s", inputStr);
+
+    if (strlen(inputStr) <= 20) {
+      break;
+    } else {
+      printf("\nERROR: Invalid input (Maximum of 20 characters)\n");
+    }
+  }
+
+  printf("\n");
+
+  for (int i = 0; i < numberOfLoops; i++) {
+    while (numberOfLoops > 0) {
+      printf("%s\n", inputStr);
+      numberOfLoops--;
+    }
+  }
+  printf("\nPress any key to retry or X to exit the program.");
+
+  char userInput = getch();
+
+  if (userInput == 'x' || userInput == 'X') {
+    return 'n';
+  } else {
+    return 'y';
+  }
+}
 
 void main () {
+  int usageCount = 0;
 
   while (1) {
     int numberOfLoops;
-    printf("How many times do you want this to loop? ");
-    scanf("%d", &numberOfLoops);
-    
-    int inputNumber;
-    printf("What number do you want to repeat? ");
-    scanf("%d", &inputNumber);
 
-    while (numberOfLoops > 0) {
-      printf("%d\n", inputNumber);
-      numberOfLoops--;
+    if (usageCount > 0) {
+      printf("\n\nHow many times do you want this to loop? (Max 10) ");
+    } else {
+      printf("How many times do you want this to loop? (Max 10) ");
     }
+    scanf("%d", &numberOfLoops);
 
-    printf("Press any key to retry or X to exit the program.\n");
+    if (numberOfLoops <= 10) {
+      char willContinue = loop(numberOfLoops);
+      usageCount++;
 
-    char userInput = getch();
-
-    if (userInput == 'x' || userInput == 'X') {
-      break;
+      if (willContinue == 'n') {
+        break;
+      }
+    } else {
+      printf("\nERROR: Invalid input (Maximum value is 10)\n\n");
     }
   }
 }
