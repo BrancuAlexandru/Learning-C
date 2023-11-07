0c0131646a2d78506d7683b82aed360e80fd25f1
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-11-07 13:58:28 +0200

    Now using better method to clear scanf() input buffer, solved 2 bugs.

--- project1/starter.c
+++ project1/starter.c
@@ -4,12 +4,14 @@
 
 char loop (int numberOfLoops) {
   char inputStr[20];
+  char inputTest[999];
 
   while (1) {
     printf("\nWhat word do you want to repeat? (Max 20 characters) ");
-    scanf("%s", inputStr);
+    scanf("%s", &inputTest);
 
-    if (strlen(inputStr) <= 20) {
+    if (strlen(inputTest) <= 20) {
+      strcpy(inputStr, inputTest);
       break;
     } else {
       printf("\nERROR: Invalid input (Maximum of 20 characters)\n");
@@ -47,16 +49,15 @@ void main () {
       printf("How many times do you want this to loop? (Max 10) ");
     }
 
+      // If input is invalid, reset the input buffer.
     if (scanf("%d", &numberOfLoops) < 1) {
-      
       if (usageCount > 0) {
         printf("\nERROR: Invalid input (Not a number)\n\n");
         usageCount--;
       } else {
         printf("\nERROR: Invalid input (Not a number)\n\n");
       }
-      char dummy;
-      scanf("%c", &dummy);
+      fflush(stdin);
     } else {
       if (numberOfLoops <= 10) {
         char willContinue = loop(numberOfLoops);
@@ -66,7 +67,11 @@ void main () {
           break;
         }
       } else {
-        printf("\nERROR: Invalid input (Maximum value is 10)\n\n");
+        if (usageCount > 0) {
+          printf("\nERROR: Invalid input (Maximum value is 10)");
+        } else {
+          printf("\nERROR: Invalid input (Maximum value is 10)\n\n");
+        }
       }
     }
   }
